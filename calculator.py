5aa4467dcd8218b5cd209e416b1a2cf673a846ca
Anish Kr Singh <59671878+anishfyi@users.noreply.github.com>
2025-03-20 18:33:42 +0530

    handle newline

--- calculator.py
+++ calculator.py
@@ -2,6 +2,10 @@ class StringCalculator:
     def add(self, numbers):
         if numbers == "":
             return 0
+        
+        # Replace newlines with commas
+        numbers = numbers.replace("\n", ",")
+        
         if "," in numbers:
             parts = numbers.split(",")
             return sum(int(num) for num in parts)
