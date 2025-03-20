58eb2ade346c22d85bf87028cb8f607e8ff87463
Anish Kr Singh <59671878+anishfyi@users.noreply.github.com>
2025-03-20 18:29:58 +0530

    unit test for calculator

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+import pytest
+from calculator import StringCalculator
+
+def test_empty_string():
+    calculator = StringCalculator()
+    assert calculator.add("") == 0
+
+def test_single_number():
+    calculator = StringCalculator()
+    assert calculator.add("1") == 1 
