c6dd291d5dbe1f5eef5e53f51efd86bfe8c0f91d
Anish Kr Singh <59671878+anishfyi@users.noreply.github.com>
2025-03-24 09:22:39 +0530

    tests for negative number validation

--- test_calculator.py
+++ test_calculator.py
@@ -22,6 +22,17 @@ class TestStringCalculator(unittest.TestCase):
     
     def test_custom_delimiter(self):
         self.assertEqual(self.calculator.add("//;\n1;2"), 3)
+    
+    # Tests for negative numbers
+    def test_negative_number(self):
+        with self.assertRaises(ValueError) as context:
+            self.calculator.add("1,-2")
+        self.assertEqual(str(context.exception), "negatives not allowed: -2")
+    
+    def test_multiple_negative_numbers(self):
+        with self.assertRaises(ValueError) as context:
+            self.calculator.add("1,-2,3,-4,-5")
+        self.assertEqual(str(context.exception), "negatives not allowed: -2, -4, -5")
 
 if __name__ == "__main__":
     unittest.main() 
